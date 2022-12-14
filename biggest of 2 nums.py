9539830aef757baf739fbae47d86605479e1d0cc
rathan-code <120500645+rathan-code@users.noreply.github.com>
2022-12-14 23:00:59 +0530

    Update biggest of 2 nums.py

--- biggest of 2 nums.py
+++ biggest of 2 nums.py
@@ -1,6 +1,7 @@
+#To print the Biggest number
 #while entering two values please separate with a space
 # eg:- 2(space)3 
-a,b=[int(a) for a in input ("Enter two Values: ").split()]
+a,b=[int(a) for a in input ("Enter two Values: ").split()] #we can continue same for input of n values using a,b,c and so on...
 if a>b:
     print("is the biggest number",a)
 else:
