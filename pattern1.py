f8b2704231fc801243adc15262b1a4f412db946e
rathan-code <120500645+rathan-code@users.noreply.github.com>
2022-12-20 23:15:42 +0530

    Add files via upload

---
+++ pattern1.py
@@ -0,0 +1,15 @@
+num=int(input("enter the number of rows"))
+for i in range(1,num+1):
+    for j in range(1,i+1):
+        print("+",end=" ")
+    print()
+
+    '''
+    out put
+    enter the number of rows 5
+    +
+    + +
+    + + +
+    + + + +
+    + + + + +
+    '''
