28e4efdbda23654a3922f5e9acdff825801ecd21
rathan-code <120500645+rathan-code@users.noreply.github.com>
2022-12-22 22:50:35 +0530

    Add files via upload

---
+++ pattern2.py
@@ -0,0 +1,14 @@
+a=int(input("Number of rows"))
+k=1
+for  i in range (1,a+1):
+    for j in range (1,k+1):
+        print("*",end=" ")
+    k=k+2
+    print()
+    '''
+    out put 
+    * 
+    * * *
+    * * * * *
+    * * * * * * *
+    '''
