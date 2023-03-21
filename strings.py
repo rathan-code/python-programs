694b5fd5f2ddb433e793d58e408586bf4b323011
Rathan Guntuka <120500645+rathan-code@users.noreply.github.com>
2023-03-21 12:32:19 +0530

    Practice file

---
+++ strings.py
@@ -0,0 +1,48 @@
+# course="Python"
+# print(len(course))
+# print(course[0])
+# print(course[-1])
+# print(course[0:3])
+# print(course[0:])
+# print(course[:3])
+# print(course[:])
+
+#Escape Sequences
+
+        # \" 
+        # \'
+        # \\
+        # \n
+# cour= "Python\nPrograms"
+# cour1="Python\"Programs"
+# cour2="Python\\Programs"
+# cour3="Python\'Programs"
+# print(cour)
+# print(cour1)
+# print(cour2)
+# print(cour3)
+# print(cour1[7:10])
+
+#Formatted Strings 
+
+# first="Rathan"
+# last="Guntuka"
+# print(first + " " + last) # Cancatenation
+#               #or
+# print(f"{first} {last}")  #Formatted String
+# print(f"{len(first)} {last}")
+
+#String Methods
+
+course="  Python Programming"
+print(course.lower())
+print(course.upper())
+print(course.title())
+print(course.strip()) # removes any voids spaces or Extra spaces in the string( removes only at ends)
+print(course.rstrip()) #removes right side spaces
+print(course.lstrip()) # remves left side spaces
+
+
+
+
+
